6aa3a037fc5918d242daf6540dca207a0cc9035c
Insane <luojieyy@gmail.com>
2015-12-04 11:49:20 +0800

    修改模板

--- www/js/app.js
+++ www/js/app.js
@@ -1,65 +1,64 @@
 var app = angular.module('myApp', ['ionic']);
- 
+
 app.config(function($stateProvider, $urlRouterProvider) {
-  $stateProvider
-    .state('tabs', {
-      url: '/tab',
-      controller: 'TabsCtrl',
-      templateUrl: 'templates/tabs.html'
-    })
-    .state('tabs.home', {
-      url: '/home',
-      views: {
-        'home-tab': {
-          templateUrl: 'home.html',
-          controller: 'HomeTabCtrl'
-        }
-      }
-    })  
-    .state('tabs.settings', {
-      url: '/settings',
-      views: {
-        'settings-tab': {
-          templateUrl: 'settings.html'
-        }
-      }
-    })    
-    .state('tabs.about', {
-      url: '/about',
-      views: {
-        'about-tab': {
-	      controller: 'AboutCtrl',
-	      templateUrl: 'about.html'
-        }
-      }
-    })
-    .state('tabs.device', {
-      url: '/about',
-      views: {
-        'device-tab': {
-	      controller: 'AboutCtrl',
-	      templateUrl: 'about.html'
-        }
-      }
-    });
- 
-  $urlRouterProvider.otherwise('/tab');
+    $stateProvider
+        .state('tabs', {
+            url: '/app',
+            controller: 'TabsCtrl',
+            templateUrl: 'templates/tabs.html',
+            abstract: true
+        })
+        .state('tabs.home', {
+            url: '/home',
+            views: {
+                'home-tab': {
+                    templateUrl: 'templates/login.html'
+                }
+            }
+        })
+        .state('tabs.analyze', {
+            url: '/analyze',
+            views: {
+                'analyze-tab': {
+                    templateUrl: 'templates/analyze.html'
+                }
+            }
+        })
+        .state('tabs.device', {
+            url: '/device',
+            views: {
+                'device-tab': {
+                    controller: 'AboutCtrl',
+                    templateUrl: 'templates/device.html'
+                }
+            }
+        })
+        .state('tabs.charge', {
+            url: '/charge',
+            views: {
+                'charge-tab': {
+                    controller: 'AboutCtrl',
+                    templateUrl: 'templates/charge.html'
+                }
+            }
+        });
+
+    $urlRouterProvider.otherwise('/app/home');
 });
- 
-app.controller('TabsCtrl', function($scope, $ionicSideMenuDelegate) {
-   
-  $scope.openMenu = function () {
-    $ionicSideMenuDelegate.toggleLeft();
-  }
-   
-});
- 
-app.controller('HomeTabCtrl', function($scope, $ionicSideMenuDelegate) {
- 
-});
- 
-app.controller('AboutCtrl', function($scope, $ionicSideMenuDelegate) {
-  $scope.openMenu = function () {
-    $ionicSideMenuDelegate.toggleLeft();
-  }
-});
+
+app
+	.controller('TabsCtrl', function($scope, $ionicSideMenuDelegate, $ionicTabsDelegate) {
+	    $scope.toggleLeft = function() {
+	        $ionicSideMenuDelegate.toggleLeft();
+	    };
+
+	    $scope.changeTab = function(index){
+	    	console.log('tab: ', index);
+	    	$ionicTabsDelegate.$getByHandle('tabs').select(index);
+	    }
+	})
+	.controller('HomeTabCtrl', function($scope, $ionicSideMenuDelegate) {
+
+	}).controller('AboutCtrl', function($scope, $ionicSideMenuDelegate) {
+  
+	});
